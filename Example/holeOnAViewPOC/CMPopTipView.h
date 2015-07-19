a20952ff6cadb302d5947656c902b4a8f4baa0c9
jgurnani <jaygurnani@JAys-MacBook-Pro.local>
2015-07-20 00:05:01 +0530

    Managing files

--- Example/holeOnAViewPOC/CMPopTipView.h
+++ Example/holeOnAViewPOC/CMPopTipView.h
@@ -1,90 +1,30 @@
 //
-//  CMPopTipView.h
+//  JGTourTip.h
+//The MIT License (MIT)
 //
-//  Created by Chris Miles on 18/07/10.
-//  Copyright (c) Chris Miles 2010-2014.
+//Copyright (c) 2015 jgurnani
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//  
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//  
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
 //
 
-/*
-	Version: 2.2
- */
 
-
-/** \brief	Display a speech bubble-like popup on screen, pointing at the
-			designated view or button.
- 
-	A UIView subclass drawn using core graphics. Pops up (optionally animated)
-	a speech bubble-like view on screen, a rounded rectangle with a gradiant
-	fill containing a specified text message, drawn with a pointer dynamically
-	positioned to point at the center of the designated button or view.
- 
- Example 1 - point at a UIBarButtonItem in a nav bar:
- 
-	- (void)showPopTipView {
-		NSString *message = @"Start by adding a waterway to your favourites.";
-		CMPopTipView *popTipView = [[CMPopTipView alloc] initWithMessage:message];
-		popTipView.delegate = self;
-		[popTipView presentPointingAtBarButtonItem:self.navigationItem.rightBarButtonItem animated:YES];
-		
-		self.myPopTipView = popTipView;
-		[popTipView release];
-	}
-
-	- (void)dismissPopTipView {
-		[self.myPopTipView dismissAnimated:NO];
-		self.myPopTipView = nil;
-	}
-
- 
-	#pragma mark CMPopTipViewDelegate methods
-	- (void)popTipViewWasDismissedByUser:(CMPopTipView *)popTipView {
-		// User can tap CMPopTipView to dismiss it
-		self.myPopTipView = nil;
-	}
-
- Example 2 - pointing at a UIButton:
-
-	- (IBAction)buttonAction:(id)sender {
-		// Toggle popTipView when a standard UIButton is pressed
-		if (nil == self.roundRectButtonPopTipView) {
-			self.roundRectButtonPopTipView = [[[CMPopTipView alloc] initWithMessage:@"My message"] autorelease];
-			self.roundRectButtonPopTipView.delegate = self;
-
-			UIButton *button = (UIButton *)sender;
-			[self.roundRectButtonPopTipView presentPointingAtView:button inView:self.view animated:YES];
-		}
-		else {
-			// Dismiss
-			[self.roundRectButtonPopTipView dismissAnimated:YES];
-			self.roundRectButtonPopTipView = nil;
-		}	
-	}
-
-	#pragma mark CMPopTipViewDelegate methods
-	- (void)popTipViewWasDismissedByUser:(CMPopTipView *)popTipView {
-		// User can tap CMPopTipView to dismiss it
-		self.roundRectButtonPopTipView = nil;
-	}
- 
- */
 
 #import <UIKit/UIKit.h>
 
